c5b0e231893f4b8bebdc58a846ef142335636a2b
George-Kovalenko <kovalenkogosha@gmail.com>
2017-04-25 23:48:18 +0300

    Added services for Auto and AutoType

---
+++ common/src/main/java/com/itechart/services/AutoService.java
@@ -0,0 +1,32 @@
+package com.itechart.services;
+
+import com.itechart.entity.Auto;
+import com.itechart.repository.AutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class AutoService {
+    @Autowired
+    private AutoRepository autoRepository;
+
+    public List<Auto> findAll() {
+        return autoRepository.findAll();
+    }
+
+    public Auto findOne(Long id) {
+        return autoRepository.findOne(id);
+    }
+
+    public void save(Auto auto) {
+        autoRepository.saveAndFlush(auto);
+    }
+
+    public void delete(Long id) {
+        autoRepository.delete(id);
+    }
+}
