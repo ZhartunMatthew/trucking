fa7d2ff7229755043c6c2b5003df54530b116ef3
DinaScherbina <dinascherbina@gmail.com>
2017-04-25 15:40:42 +0300

    Add services

---
+++ common/src/main/java/com/itechart/services/InvoiceService.java
@@ -0,0 +1,33 @@
+package com.itechart.services;
+
+import com.itechart.entity.Invoice;
+import com.itechart.repository.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+    public class InvoiceService {
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    public List<Invoice> findAll(){
+        return invoiceRepository.findAll();
+    }
+
+    public  Invoice findOne(Long id) {
+        return invoiceRepository.findOne(id);
+    }
+
+    public void save(Invoice invoice){
+        invoiceRepository.saveAndFlush(invoice);
+    }
+
+    public void delete(Long id){
+        invoiceRepository.delete(id);
+    }
+}
---
+++ common/src/main/java/com/itechart/services/UserService.java
@@ -0,0 +1,33 @@
+package com.itechart.services;
+
+import com.itechart.entity.User;
+import com.itechart.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+    public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public  User findOne(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    public void save(User user){
+        userRepository.saveAndFlush(user);
+    }
+
+    public void delete(Long id){
+        userRepository.delete(id);
+    }
+}
