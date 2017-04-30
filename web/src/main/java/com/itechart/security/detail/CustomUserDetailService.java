a0cc0f7dc9e429875f9605465e639d5a8de0b7c5
ZhartunMatthew <zhartunmatthew@gmail.com>
2017-04-30 11:10:57 +0300

    Added simple authentication

---
+++ web/src/main/java/com/itechart/security/detail/CustomUserDetailService.java
@@ -0,0 +1,14 @@
+package com.itechart.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+
+public class CustomUserDetailService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
---
+++ web/src/main/java/com/itechart/security/detail/CustomUserDetails.java
@@ -0,0 +1,7 @@
+package com.itechart.security.detail;
+
+/**
+ * Created by ZhartunMatthew on 30.04.2017.
+ */
+public class CustomUserDetails {
+}
