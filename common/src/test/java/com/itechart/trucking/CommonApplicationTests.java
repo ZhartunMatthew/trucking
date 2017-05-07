081fa3524601f97becce44c9bbe975cf2c1468b7
ZhartunMatthew <zhartunmatthew@gmail.com>
2017-05-07 20:10:35 +0300

    Updated security, updated tests

--- common/src/test/java/com/itechart/trucking/CommonApplicationTests.java
+++ common/src/test/java/com/itechart/trucking/CommonApplicationTests.java
@@ -11,9 +11,5 @@ public class CommonApplicationTests {
 
 	@Test
 	public void contextLoads() {
-//		UserService userService = new UserService();
-//		User user = userService.findOne(1L);
-//		System.out.println(user);
 	}
-
 }
