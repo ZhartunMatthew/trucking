2e07c8433dbef0c6cca18f8dfc586f0aed99d53e
ZhartunMatthew <zhartunmatthew@gmail.com>
2017-05-14 00:14:01 +0300

    Implemented providers (trucking company, user, waybill)

--- web/src/main/java/com/itechart/trucking/security/providers/TruckingCompanyProvider.java
+++ web/src/main/java/com/itechart/trucking/security/providers/TruckingCompanyProvider.java
@@ -1,27 +1,43 @@
 package com.itechart.trucking.security.providers;
 
 import com.itechart.trucking.entity.enums.UserRoleEnum;
+import com.itechart.trucking.security.detail.CustomUserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TruckingCompanyProvider implements AbstractDataProvider {
     @Override
-    public boolean provideGET(UserRoleEnum role, Long companyId, Long carId) {
+    public boolean provideGET(CustomUserDetails details, Long truckingCompanyId) {
+        if(details.getRole() == UserRoleEnum.SYSTEM_ADMIN) {
+            return true;
+        } else {
+            if(truckingCompanyId == null) {
+                return false;
+            } else {
+                if(details.getTruckingCompanyId().equals(truckingCompanyId)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean providePOST(CustomUserDetails details, Long truckingCompanyId) {
+        if(details.getRole() == UserRoleEnum.SYSTEM_ADMIN) {
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean providePOST(UserRoleEnum role, Long companyId, Long carId) {
+    public boolean providePUT(CustomUserDetails details, Long truckingCompanyId) {
         return false;
     }
 
     @Override
-    public boolean providePUT(UserRoleEnum role, Long companyId, Long carId) {
-        return false;
-    }
-
-    @Override
-    public boolean provideDELETE(UserRoleEnum role, Long companyId, Long carId) {
+    public boolean provideDELETE(CustomUserDetails details, Long truckingCompanyId) {
         return false;
     }
 }
--- web/src/main/java/com/itechart/trucking/security/providers/WaybillProvider.java
+++ web/src/main/java/com/itechart/trucking/security/providers/WaybillProvider.java
@@ -1,27 +1,88 @@
 package com.itechart.trucking.security.providers;
 
+import com.itechart.trucking.entity.Invoice;
+import com.itechart.trucking.entity.Waybill;
 import com.itechart.trucking.entity.enums.UserRoleEnum;
+import com.itechart.trucking.security.detail.CustomUserDetails;
+import com.itechart.trucking.services.InvoiceService;
+import com.itechart.trucking.services.WaybillService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WaybillProvider implements AbstractDataProvider {
+
+    @Autowired
+    private WaybillService waybillService;
+
+    @Autowired
+    private InvoiceService invoiceService;
+
     @Override
-    public boolean provideGET(UserRoleEnum role, Long companyId, Long carId) {
+    public boolean provideGET(CustomUserDetails details, Long waybillId) {
+        UserRoleEnum role = details.getRole();
+        if(role == UserRoleEnum.MANAGER || role == UserRoleEnum.DRIVER) {
+            if(waybillId == null) {
+                return true;
+            }
+            //проверяем, что waybill принадлежит нашей компании
+            Waybill waybill = waybillService.securedFindOne(waybillId);
+            Invoice invoice = invoiceService.securedFindOne(waybill.getInvoice().getId());
+            if(invoice.getTruckingCompany().getId().equals(details.getTruckingCompanyId())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
         return false;
     }
 
     @Override
-    public boolean providePOST(UserRoleEnum role, Long companyId, Long carId) {
+    public boolean providePOST(CustomUserDetails details, Long waybillId) {
+        if(details.getRole() == UserRoleEnum.MANAGER) {
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean providePUT(UserRoleEnum role, Long companyId, Long carId) {
+    public boolean providePUT(CustomUserDetails details, Long waybillId) {
+        if(waybillId == null) {
+            return false;
+        }
+        UserRoleEnum role = details.getRole();
+        if(role == UserRoleEnum.MANAGER || role == UserRoleEnum.DRIVER) {
+            Waybill waybill = waybillService.securedFindOne(waybillId);
+            Invoice invoice = invoiceService.securedFindOne(waybill.getInvoice().getId());
+            if(invoice.getTruckingCompany().getId().equals(details.getTruckingCompanyId())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
         return false;
     }
 
     @Override
-    public boolean provideDELETE(UserRoleEnum role, Long companyId, Long carId) {
+    public boolean provideDELETE(CustomUserDetails details, Long waybillId) {
+        if(details.getRole() == UserRoleEnum.MANAGER) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean provideGETbyTruckingId(CustomUserDetails details, Long truckingId) {
+        UserRoleEnum role = details.getRole();
+        if(role == UserRoleEnum.MANAGER || role == UserRoleEnum.DRIVER) {
+            if(truckingId == null) {
+                return false;
+            }
+            if(truckingId.equals(details.getTruckingCompanyId())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
         return false;
     }
 }
