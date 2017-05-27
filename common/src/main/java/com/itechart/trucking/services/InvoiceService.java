475dba276a716ab920070648bd5649cde88390ee
George-Kovalenko <kovalenkogosha@gmail.com>
2017-05-27 18:53:48 +0300

    Added updating of checkDate and managerId in Invoice

--- common/src/main/java/com/itechart/trucking/services/InvoiceService.java
+++ common/src/main/java/com/itechart/trucking/services/InvoiceService.java
@@ -10,48 +10,55 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 public class InvoiceService {
 
     @Autowired
     private InvoiceRepository invoiceRepository;
 
     @PreAuthorize("hasPermission(null , 'Invoice', 'GET')")
+    @Transactional(readOnly = true)
     public List<Invoice> findAll() {
         return invoiceRepository.findAll();
     }
 
     @PreAuthorize("hasPermission(#id, 'Invoice', 'GET')")
+    @Transactional(readOnly = true)
     public Invoice findOne(Long id) {
         return invoiceRepository.findOne(id);
     }
 
     @PreAuthorize("hasPermission(null, 'Invoice', 'POST') or hasPermission(#invoice.id, 'Invoice', 'PUT')")
+    @Transactional
     public Invoice save(Invoice invoice) {
         return invoiceRepository.saveAndFlush(invoice);
     }
 
     @PreAuthorize("hasPermission(#id, 'Invoice', 'DELETE')")
+    @Transactional
     public void delete(Long id) {
         invoiceRepository.delete(id);
     }
 
     @PreAuthorize("hasPermission(#id, 'Invoice', 'GET')")
+    @Transactional(readOnly = true)
     public List<Invoice> findByTruckingCompanyId(Long id) {
         return invoiceRepository.findAllByTruckingCompany_Id(id);
     }
 
     @PreAuthorize("hasPermission(#id, 'Invoice', 'GET')")
+    @Transactional(readOnly = true)
     public Invoice findByIdAndTruckingCompanyId(Long id, Long trId) {
         return invoiceRepository.findByIdAndTruckingCompany_Id(id, trId);
     }
 
     @PreAuthorize("hasAnyRole('DRIVER', 'MANAGER')")
+    @Transactional(readOnly = true)
     public Invoice findByWaybillId(Long id) {
         return invoiceRepository.findByWaybill_Id(id);
     }
 
     @PreAuthorize("hasAnyRole('DISPATCHER', 'MANAGER', 'DRIVER', 'COMPANY_OWNER')")
+    @Transactional(readOnly = true)
     public Invoice securedFindOne(Long id) {
         return invoiceRepository.findOne(id);
     }
