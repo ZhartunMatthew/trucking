a149c0b7074b369d3f8f89a707d0e7bed5979905
ZhartunMatthew <zhartunmatthew@gmail.com>
2017-05-28 15:22:39 +0300

    Protection from undefined list of products

--- web/src/main/frontend/src/components/invoice/invoice-form.js
+++ web/src/main/frontend/src/components/invoice/invoice-form.js
@@ -12,11 +12,11 @@ class InvoiceForm extends React.Component {
   }
 
   handleRegisterDate(event) {
-    this.props.updateOperation('register-date', event.target.value);
+    this.props.updateOperation('registerDate', event.target.value);
   }
 
   handleCustomerCompany(event) {
-    this.props.updateOperation('customer-company', event.target.value);
+    this.props.updateOperation('customerCompany', event.target.value);
   }
 
   save() {
@@ -29,19 +29,23 @@ class InvoiceForm extends React.Component {
 
   render() {
     let editingLabel = <span> Check invoice №<b>{this.props.invoice.number}</b> </span>;
+    let role = this.props.userRole;
+    let disableEditing = role !== "DISPATCHER";
+
     return (
       <div>
         <form className='form-horizontal'>
           <fieldset>
-            <legend>{editingLabel} </legend>
+            <legend> {editingLabel} </legend>
             <Input id='number' type='text' label='Invoice number' placeholder='' value={this.props.invoice.number || ''}
-                   onChange={this.handleNumberChange.bind(this)} readOnly={true}/>
-            <Input id='register-date' type='text' label='Register date' placeholder=''
+                   onChange={this.handleNumberChange.bind(this)}
+                   readOnly={disableEditing}/>
+            <Input id='registerDate' type='text' label='Register date' placeholder=''
                    value={this.props.invoice.registerDate || ''} onChange={this.handleRegisterDate.bind(this)}
-                   readOnly={true}/>
-            <Input id='customer-company' type='text' label='Customer company' placeholder=''
+                   readOnly={disableEditing}/>
+            <Input id='customerCompany' type='text' label='Customer company' placeholder=''
                    value={this.props.invoice.customerCompany || ''} onChange={this.handleCustomerCompany.bind(this)}
-                   readOnly={true}/>
+                   readOnly={disableEditing}/>
             <div className='btn-toolbar text-center'>
               <div className='btn-group' role='group'>
                 <button type='button' className='btn btn-success' onClick={this.cancel.bind(this)}>Close</button>
@@ -62,11 +66,14 @@ InvoiceForm.propTypes = {
   invoice: React.PropTypes.object.isRequired,
   updateInvoice: React.PropTypes.func.isRequired,
   updateOperation: React.PropTypes.func.isRequired,
-  cancelOperation: React.PropTypes.func.isRequired
+  cancelOperation: React.PropTypes.func.isRequired,
+  userRole: React.PropTypes.String
 };
 
-let mapStateToProps = function () {
-  return {};
+let mapStateToProps = function (state) {
+  return {
+    userRole: state.userRole.userRole
+  };
 };
 
 function mapDispatchToProps(dispatch) {
