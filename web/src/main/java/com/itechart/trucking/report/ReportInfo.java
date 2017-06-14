fc08fb8297dff58f27c5fe7740b5213040b3347c
DinaScherbina <dinascherbina@gmail.com>
2017-06-14 20:40:22 +0300

    Add reportInfo class

---
+++ web/src/main/java/com/itechart/trucking/report/ReportInfo.java
@@ -0,0 +1,162 @@
+package com.itechart.trucking.report;
+
+public class ReportInfo {
+
+    private Long carsAmount;
+    private Integer employeesAmount;
+    private Integer invoiceAmount;
+    private Integer customersAmount;
+
+    private Integer productsSum;
+    private Integer productDelivered;
+    private Integer productLost;
+    private Double productLostPercent;
+    private Double productLostPrice;
+
+    private Double avgDistance;
+    private Double totalDistance;
+    private Double fuelPrice;
+
+    private Double avgInvoiceRevenue;
+    private Double totalInvoiceRevenue;
+
+    private Double income;
+    private Double outcome;
+    private Double revenue;
+
+    public Long getCarsAmount() {
+        return carsAmount;
+    }
+
+    public void setCarsAmount(Long carsAmount) {
+        this.carsAmount = carsAmount;
+    }
+
+    public Integer getEmployeesAmount() {
+        return employeesAmount;
+    }
+
+    public void setEmployeesAmount(Integer employeesAmount) {
+        this.employeesAmount = employeesAmount;
+    }
+
+    public Integer getInvoiceAmount() {
+        return invoiceAmount;
+    }
+
+    public void setInvoiceAmount(Integer invioceAmount) {
+        this.invoiceAmount = invioceAmount;
+    }
+
+    public Integer getCustomersAmount() {
+        return customersAmount;
+    }
+
+    public void setCustomersAmount(Integer customersAmount) {
+        this.customersAmount = customersAmount;
+    }
+
+    public Integer getProductsSum() {
+        return productsSum;
+    }
+
+    public void setProductsSum(Integer productsSum) {
+        this.productsSum = productsSum;
+    }
+
+    public Integer getProductDelivered() {
+        return productDelivered;
+    }
+
+    public void setProductDelivered(Integer productDelivered) {
+        this.productDelivered = productDelivered;
+    }
+
+    public Integer getProductLost() {
+        return productLost;
+    }
+
+    public void setProductLost(Integer productLost) {
+        this.productLost = productLost;
+    }
+
+    public Double getProductLostPercent() {
+        return productLostPercent;
+    }
+
+    public void setProductLostPercent(Double productLostPercent) {
+        this.productLostPercent = productLostPercent;
+    }
+
+    public Double getProductLostPrice() {
+        return productLostPrice;
+    }
+
+    public void setProductLostPrice(Double productLostPrice) {
+        this.productLostPrice = productLostPrice;
+    }
+
+    public Double getAvgDistance() {
+        return avgDistance;
+    }
+
+    public void setAvgDistance(Double avgDistance) {
+        this.avgDistance = avgDistance;
+    }
+
+    public Double getTotalDistance() {
+        return totalDistance;
+    }
+
+    public void setTotalDistance(Double totalDistance) {
+        this.totalDistance = totalDistance;
+    }
+
+    public Double getFuelPrice() {
+        return fuelPrice;
+    }
+
+    public void setFuelPrice(Double fuelPrice) {
+        this.fuelPrice = fuelPrice;
+    }
+
+    public Double getAvgInvoiceRevenue() {
+        return avgInvoiceRevenue;
+    }
+
+    public void setAvgInvoiceRevenue(Double avgInvoiceRevenue) {
+        this.avgInvoiceRevenue = avgInvoiceRevenue;
+    }
+
+    public Double getTotalInvoiceRevenue() {
+        return totalInvoiceRevenue;
+    }
+
+    public void setTotalInvoiceRevenue(Double totalInvoiceRevenue) {
+        this.totalInvoiceRevenue = totalInvoiceRevenue;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public Double getOutcome() {
+        return outcome;
+    }
+
+    public void setOutcome(Double outcome) {
+        this.outcome = outcome;
+    }
+
+    public Double getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(Double revenue) {
+        this.revenue = revenue;
+    }
+}
