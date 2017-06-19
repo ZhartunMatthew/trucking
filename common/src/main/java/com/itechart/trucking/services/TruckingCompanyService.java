983c73a0ca741d03f12c71d3933c35c8d97b92f2
ZhartunMatthew <zhartunmatthew@gmail.com>
2017-06-19 03:05:23 +0300

    Fixed transaction: added transaction with saving trucking company with new user

--- common/src/main/java/com/itechart/trucking/services/TruckingCompanyService.java
+++ common/src/main/java/com/itechart/trucking/services/TruckingCompanyService.java
@@ -1,10 +1,13 @@
 package com.itechart.trucking.services;
 
 import com.itechart.trucking.entity.TruckingCompany;
+import com.itechart.trucking.entity.User;
+import com.itechart.trucking.entity.enums.UserRoleEnum;
 import com.itechart.trucking.repository.TruckingCompanyRepository;
 import com.itechart.trucking.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,6 +38,24 @@ public class TruckingCompanyService {
     @Transactional
     public TruckingCompany save(TruckingCompany truckingCompany) {
         return truckingCompanyRepository.saveAndFlush(truckingCompany);
+    }
+
+    @PreAuthorize("hasPermission(null, 'TruckingCompany', 'POST') or hasPermission(#truckingCompany.id, 'TruckingCompany', 'PUT')")
+    @Transactional
+    public TruckingCompany saveFullTruckingCompany(TruckingCompany truckingCompany) {
+        userRepository.save(createAdminForTruckingCompany(truckingCompany));
+        return truckingCompanyRepository.saveAndFlush(truckingCompany);
+    }
+
+    private User createAdminForTruckingCompany(TruckingCompany company) {
+        User user = new User();
+        user.setSurname("");
+        user.setLogin("admin" + company.getTaxpayerNumber());
+        user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+        user.setSalt("salt");
+        user.setTruckingCompany(company);
+        user.setUserRole(UserRoleEnum.ADMIN);
+        return user;
     }
 
     public TruckingCompany securedFindOne(Long id) {
