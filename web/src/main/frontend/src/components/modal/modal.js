98fbe5b625de19c5408415a702c9628a42f7c664
George-Kovalenko <kovalenkogosha@gmail.com>
2017-06-21 17:00:18 +0300

    Code cleanup in modal-component

--- web/src/main/frontend/src/components/modal/modal.js
+++ web/src/main/frontend/src/components/modal/modal.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {connect} from 'react-redux';
 
 class ModalComponent extends React.Component {
 
   render() {
     return (
-      <div id="modal-action" className="modal-action-dialog">
-        <div id="modal-content" className="panel panel-info modal-action-content">
-          <div className="panel-heading">
-            <h4 id="modal-action-text" className="text-center"> </h4>
+      <div id='modal-action' className='modal-action-dialog'>
+        <div id='modal-content' className='panel panel-info modal-action-content'>
+          <div className='panel-heading'>
+            <h4 id='modal-action-text' className='text-center'> </h4>
           </div>
-          <div className="panel-body">
-            <div id="modal-description-text" className='text-center'>
-              <div className="close"/>
+          <div className='panel-body'>
+            <div id='modal-description-text' className='text-center'>
+              <div className='close'/>
             </div>
           </div>
         </div>
@@ -21,8 +20,4 @@ class ModalComponent extends React.Component {
   }
 }
 
-let mapStateToProps = function (state) {};
-
-let mapDispatchToProps = function (dispatch) {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ModalComponent);
+export default ModalComponent;
