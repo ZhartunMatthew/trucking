d7edeeffaff0187bdc5cf447d7eb7846c42e9bf5
ZhartunMatthew <zhartunmatthew@gmail.com>
2017-07-07 22:17:35 +0300

    Updated messages dto and controller

--- web/src/main/java/com/itechart/trucking/messaging/controller/MessagingOutputController.java
+++ web/src/main/java/com/itechart/trucking/messaging/controller/MessagingOutputController.java
@@ -1,23 +1,41 @@
 package com.itechart.trucking.messaging.controller;
 
-import com.itechart.trucking.messaging.dto.InputMessage;
-import com.itechart.trucking.messaging.dto.OutputMessage;
+import com.itechart.trucking.messaging.dto.CustomMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
+@MessageMapping("/message-aggregator")
 public class MessagingOutputController {
 
-    @MessageMapping("/message-aggregator")
-    @SendTo("/topic/common-box")
-    public OutputMessage answer(@RequestBody InputMessage message) throws Exception {
-        System.out.println("MESSAGE: " + message.getText());
-        Thread.sleep(1000);
-        OutputMessage outputMessage = new OutputMessage();
-        outputMessage.setAnswer(message.getText() + "123" + message.getText());
+    private final static Logger LOGGER = LoggerFactory.getLogger(MessagingOutputController.class);
+    private final static String COMMON_BOX = "/topic/common-box";
+
+    @MessageMapping("/new-customer")
+    @SendTo(COMMON_BOX + "/dispatcher-box")
+    public CustomMessage onCustomerCreate(@RequestBody CustomMessage inputMessage) throws Exception {
+        LOGGER.info("MESSAGE FROM ADMIN: {}", inputMessage);
+        CustomMessage outputMessage = new CustomMessage();
+        outputMessage.setSubject("New customer!");
+        outputMessage.setContent("Customer " + inputMessage.getContent() + " have been added");
         return outputMessage;
     }
 
+    @MessageMapping("/new-invoice")
+    @SendTo(COMMON_BOX + "/manager-box")
+    public CustomMessage onInvoiceCreate() {
+
+        return null;
+    }
+
+    @MessageMapping("/new-waybill")
+    @SendTo(COMMON_BOX + "/driver-box")
+    public CustomMessage onWaybillCreate() {
+
+        return null;
+    }
 }
--- web/src/main/java/com/itechart/trucking/messaging/dto/CustomMessage.java
+++ web/src/main/java/com/itechart/trucking/messaging/dto/CustomMessage.java
@@ -5,6 +5,7 @@ import lombok.ToString;
 
 @Data
 @ToString
-public class InputMessage {
-    private String text;
+public class CustomMessage {
+    private String subject;
+    private String content;
 }
